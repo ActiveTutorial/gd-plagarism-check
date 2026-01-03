1192ed42568edb2c07359879a1c6bd5c4f2dd377
John Doe <johndoe@email.com>
2026-01-03 08:05:37 +0100

    All js files removed

--- gmd-api/utils.js
+++
@@ -1,8 +0,0 @@
-function parseValue(val) {
-  if (val === '1') return true;
-  if (val === '0') return false;
-  if (!isNaN(val)) return Number(val);
-  return val;
-}
-
-module.exports = { parseValue };
